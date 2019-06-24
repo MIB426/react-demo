f9e6a9b9a7a5afca1d9f42cb38f5bfeffd52ab14
James Wu <MIB426@htmail.com>
2019-06-24 16:19:33 +0800

    add img

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react"
 
 class Home extends React.Component{
+  
     render(){
         return( 
         <div><p>import {'{'} render {'}'} from 'react-dom';</p>
           <p>render({'<'}App {'/>'}, document.getElementById('root'));</p>
         <p>import reactDOM from 'reatc-dom';</p>
         <p>ReactDOM.render({'<'} App {'/>'}, document.getElementById('root'));</p>
+        <p><img src='/asset/img/img1.png'></img></p>
+        <p><img src='/asset/img/img2.png'></img></p>
+      
+     
       </div>
         )
     }
